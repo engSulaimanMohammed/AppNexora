4c0e049653b2e976559bd8957538409d67f6539b
Rashid <alshiblir650@gmail.com>
2026-08-12 17:14:09 +0400

    store the name of SchedulePolicy.java

--- src/main/java/org/example/Entities/SchedulePolicy.java
+++ src/main/java/org/example/Entities/SchedulePolicy.java
@@ -1,4 +1,14 @@
 package org.example.Entities;
 
 public class SchedulePolicy {
+
+    private String name;
+    private String startTime;
+    private String endTime;
+    private int workingDays;
+
+
+
+
+
 }
