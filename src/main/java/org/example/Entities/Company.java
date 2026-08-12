ef10d84fd374dd011e1989dfbd1049c96ee83aa5
mohammed <mooodo150@gmail.com>
2026-08-12 17:19:31 +0400

    Generate getters and setters for domain entities

--- src/main/java/org/example/Entities/Company.java
+++ src/main/java/org/example/Entities/Company.java
@@ -12,6 +12,38 @@ public class Company {
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
     public Company(Long id, String name, String userRole, int employeeCount) {
 
         this.id = id;
