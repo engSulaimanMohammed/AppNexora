31d6f7a981d5da626964566d869ca22b01b18a93
Rashid <alshiblir650@gmail.com>
2026-08-12 17:16:16 +0400

    creat new object

--- src/main/java/org/example/Entities/SchedulePolicy.java
+++ src/main/java/org/example/Entities/SchedulePolicy.java
@@ -7,6 +7,16 @@ public class SchedulePolicy {
     private String endTime;
     private int workingDays;
 
+    public SchedulePolicy(String name, String startTime,
+                          String endTime, int workingDays) {
+
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.workingDays = workingDays;
+    }
+
+
 
 
 
