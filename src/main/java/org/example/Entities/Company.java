d6686df6c65f216f81973b89cf85b39a49263a3c
mohammed <mooodo150@gmail.com>
2026-08-12 17:08:14 +0400

    I Creates an empty Company object

--- src/main/java/org/example/Entities/Company.java
+++ src/main/java/org/example/Entities/Company.java
@@ -1,4 +1,11 @@
 package org.example.Entities;
 
 public class Company {
+
+    private Long id;
+    private String name;
+    private String userRole;
+    private int employeeCount;
+    public Company() {
+    }
 }
