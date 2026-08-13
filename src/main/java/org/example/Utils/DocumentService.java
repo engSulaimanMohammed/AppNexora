f19eef99ba0679d2cb079665e0f803e9b555c873
mohammed <mooodo150@gmail.com>
2026-08-13 10:32:11 +0400

    feat(server):create class schedulePolicyService

--- src/main/java/org/example/Utils/DocumentService.java
+++ src/main/java/org/example/Utils/DocumentService.java
@@ -1,12 +1,10 @@
-package org.example.Entities;
+package org.example.Utils;
 
-public class Document {
+import org.example.Entities.Document;
+
+public class DocumentService {
     public void uploadDocument(Document document) {
         // Store uploaded employee files
         System.out.println("File uploaded: " + document.getFileName());
-    }
-
-    private String getFileName() {
-        return "";
     }
 }
