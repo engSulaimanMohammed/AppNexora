6f3e87550250ae7b674291aaff6435c0c86c8522
Rashid <alshiblir650@gmail.com>
2026-08-13 08:47:41 +0400

    Add getters for Document fields

--- src/main/java/org/example/Entities/Document.java
+++ src/main/java/org/example/Entities/Document.java
@@ -1,4 +1,38 @@
 package org.example.Entities;
 
 public class Document {
+
+    private int id;
+    private int employeeId;
+    private String name;
+    private String type;
+
+    public Document(int id, int employeeId,
+                    String name, String type) {
+
+        this.id = id;
+        this.employeeId = employeeId;
+        this.name = name;
+        this.type = type;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
 }
+
