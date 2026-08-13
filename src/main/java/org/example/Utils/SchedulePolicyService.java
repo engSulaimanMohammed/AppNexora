7b2a4db6657a32f83aff094fae9ed12026675715
mohammed <mooodo150@gmail.com>
2026-08-13 10:30:31 +0400

    feat(server):create class schedulePolicyService

---
+++ src/main/java/org/example/Utils/SchedulePolicyService.java
@@ -0,0 +1,8 @@
+package org.example.Utils;
+
+public class SchedulePolicyService {
+    public void createPolicy(SchedulePolicy policy) {
+        // Define work shift parameters
+        System.out.println("Shift policy created: " + policy.getPolicyName());
+    }
+}
