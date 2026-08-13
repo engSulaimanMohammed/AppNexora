869efa7795584efc7b2fff5cfbd382dc4a213093
mohammed said <mohed.said77@gmail.com>
2026-08-13 09:29:02 +0400

    i store y=the depatmentname and managername an location as String

--- src/main/java/org/example/Entities/Department.java
+++ src/main/java/org/example/Entities/Department.java
@@ -3,3 +3,7 @@ package org.example.Entities;
 public class Department {
 
     private int departmentId;
+    private String departmentName;
+    private String managerName;
+    private String location;
+
