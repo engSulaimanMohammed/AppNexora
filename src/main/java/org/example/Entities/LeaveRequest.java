15382500fc66b71d01a56956ab47461e4430b0f3
mohammed <mooodo150@gmail.com>
2026-08-13 09:16:53 +0400

    I used setter and getter method

--- src/main/java/org/example/Entities/LeaveRequest.java
+++ src/main/java/org/example/Entities/LeaveRequest.java
@@ -7,6 +7,39 @@ public class LeaveRequest {
     private String leaveType;
     private String status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
     public LeaveRequest() {
+
     }
 }
