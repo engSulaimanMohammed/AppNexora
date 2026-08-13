693e9c28c7f6ff717bf357f5d5060b89a7d9118c
Rashid <alshiblir650@gmail.com>
2026-08-13 09:47:40 +0400

    Implement account access operations

--- src/main/java/org/example/Services/AccountAccessServiceImpl.java
+++ src/main/java/org/example/Services/AccountAccessServiceImpl.java
@@ -1,9 +1,11 @@
 package org.example.Services;
 
+import org.example.Interfaces.AccountAccessService;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class AccountAccessServiceImpl implements AccountAccessService{
+public class AccountAccessServiceImpl implements AccountAccessService {
 
     private final Map<String, String> accounts = new HashMap<>();
 
@@ -12,4 +14,23 @@ public class AccountAccessServiceImpl implements AccountAccessService{
     }
 
 
+    @Override
+    public boolean login(String username, String password) {
+        return accounts.containsKey(username)
+                && accounts.get(username).equals(password);
+    }
+
+    @Override
+    public void addAccount(String username, String password) {
+        if (accountExists(username)) {
+            throw new IllegalArgumentException(
+                    "Username already exists.");
+
+        }
+    }
+
+    @Override
+    public boolean accountExists(String username) {
+        return accounts.containsKey(username);
+    }
 }
