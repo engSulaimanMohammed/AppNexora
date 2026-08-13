36b750b5679dcc3e68769672420dde420f5a1028
Muhammad Daniyal Mehmood <daniyalazeemi@yahoo.com>
2026-08-13 12:26:38 +0400

    Fixed bugs
    - Refactored code
    - Fixed config
    - Fixed Menu
    - Added Menu option

--- src/main/java/org/example/Entities/Employee.java
+++ src/main/java/org/example/Entities/Employee.java
@@ -1,7 +1,5 @@
 package org.example.Entities;
 
-public class Employee {
-}
 
 public class Employee extends User {
 
@@ -11,4 +9,8 @@ public class Employee extends User {
     private String department;
     private String position;
     private double leaveBalance;
+
+    public Employee(int id, String username, String password, String role, String email) {
+        super(id, username, password, role, email);
     }
+}
