21116244ee945429559f2ecc2f07158238331c3c
Rashid <alshiblir650@gmail.com>
2026-08-13 10:02:07 +0400

    Add EmployeeService dependency to ReportService

---
+++ src/main/java/org/example/Services/ReportServiceAdmin.java
@@ -0,0 +1,10 @@
+package org.example.Services;
+
+public class ReportServiceAdmin {
+
+    private final EmployeeService employeeService;
+
+    public ReportServiceAdmin(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
