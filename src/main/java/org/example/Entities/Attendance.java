3591342c5796afc89b2035c0acb9436ed16af7c4
mohammed <mooodo150@gmail.com>
2026-08-13 10:19:53 +0400

    feat(server):I add override

--- src/main/java/org/example/Entities/Attendance.java
+++ src/main/java/org/example/Entities/Attendance.java
@@ -1,4 +1,6 @@
 package org.example.Entities;
 
 public class Attendance {
+    public Attendance(long l, Long employeeId, String date, String present) {
+    }
 }
--- src/main/java/org/example/Interfaces/AttendanceService.java
+++ src/main/java/org/example/Interfaces/AttendanceService.java
@@ -1,8 +1,13 @@
 package org.example.Interfaces;
 
+import org.example.Entities.Attendance;
 import org.example.Entities.AttendanceAdmin;
 
 public interface AttendanceService implements AttendanceAdmin {
+    default void recordClockIn() {
+        recordClockIn(null);
+    }
+
     @Override
     public void recordClockIn(Long employeeId) {
         // Implementation goes here.
