3764579d1d4f3a2bfb4d008d468a9ab42b88390b
Rashid <alshiblir650@gmail.com>
2026-08-13 10:10:17 +0400

    Add employee data retrieval for report

--- src/main/java/org/example/Services/ReportServiceAdmin.java
+++ src/main/java/org/example/Services/ReportServiceAdmin.java
@@ -1,5 +1,10 @@
 package org.example.Services;
 
+import org.example.Entities.Employee;
+import org.example.Entities.Report;
+
+import java.util.List;
+
 public class ReportServiceAdmin {
 
     private final EmployeeService employeeService;
@@ -7,4 +12,10 @@ public class ReportServiceAdmin {
     public ReportServiceAdmin(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-}
+
+    public Report generateEmployeeReport() {
+
+        List<Employee> employees =
+                employeeService.getAllEmployees();
+
+    }
