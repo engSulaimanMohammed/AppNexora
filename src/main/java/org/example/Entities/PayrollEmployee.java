af4f793d7ac880ebed283def35f2c551a64cdc3b
mohammed said <mohed.said77@gmail.com>
2026-08-13 09:40:26 +0400

    ID of the employee who owns this payroll

--- src/main/java/org/example/Entities/PayrollEmployee.java
+++ src/main/java/org/example/Entities/PayrollEmployee.java
@@ -3,3 +3,32 @@ package org.example.Entities;
 public class PayrollEmployee {
 
     private int payrollId;
+    private int employeeId;
+
+    }
+
+    public double getAllowances() {
+        return allowances;
+    }
+
+    public void setAllowances(double allowances) {
+        this.allowances = allowances;
+        calculateNetSalary();
+    }
+
+    public double getDeductions() {
+        return deductions;
+    }
+
+    public void setDeductions(double deductions) {
+        this.deductions = deductions;
+        calculateNetSalary();
+    }
+
+    public double getNetSalary() {
+        return netSalary;
+    }
+
+    private void calculateNetSalary() {
+        netSalary = basicSalary + allowances - deductions;
+    }
