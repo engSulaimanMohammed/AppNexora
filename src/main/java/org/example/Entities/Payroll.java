bb0681cc42cda13bd703a01b8027a0ee6a287d5e
mohammed <mooodo150@gmail.com>
2026-08-13 09:36:30 +0400

    I stored the id,name,email and department

--- src/main/java/org/example/Entities/Payroll.java
+++ src/main/java/org/example/Entities/Payroll.java
@@ -9,4 +9,44 @@ public class Payroll {
     private double deductions;
     public Payroll() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getDeductions() {
+        return deductions;
+    }
+
+    public void setDeductions(double deductions) {
+        this.deductions = deductions;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
 }
