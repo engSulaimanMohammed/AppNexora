2a41ebb1efda5e91a0a48ef41ed6468076a8817a
mohammed <mooodo150@gmail.com>
2026-08-13 09:09:11 +0400

    I used setter and getter method

--- src/main/java/org/example/Entities/AttendanceAdmin.java
+++ src/main/java/org/example/Entities/AttendanceAdmin.java
@@ -9,4 +9,36 @@ public class AttendanceAdmin {
 
     public AttendanceAdmin() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
 }
