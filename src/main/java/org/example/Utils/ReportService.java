7e3f985116e6315c1ece28f16a698a5fca563e3c
mohammed <mooodo150@gmail.com>
2026-08-13 10:33:08 +0400

    feat(server):[ompile system analytics and export opera

---
+++ src/main/java/org/example/Utils/ReportService.java
@@ -0,0 +1,8 @@
+package org.example.Utils;
+
+public class ReportService {
+    public void generateReport(String reportType) {
+        // Compile system analytics and export operational reports
+        System.out.println("Generating report: " + reportType);
+    }
+}
