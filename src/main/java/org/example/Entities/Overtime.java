6b5319fd19f8fdebdaff0fbbcd2cfbc8ffcf1dfc
mohammed said <mohed.said77@gmail.com>
2026-08-13 10:33:51 +0400

    feat(server): add employee overtime

--- src/main/java/org/example/Entities/Overtime.java
+++ src/main/java/org/example/Entities/Overtime.java
@@ -1,4 +1,77 @@
 package org.example.Entities;
 
+import java.time.LocalDate;
+
 public class Overtime {
+
+    private int overtimeId;
+    private int employeeId;
+    private LocalDate date;
+    private double hours;
+    private String reason;
+    private String status;
+
+    public Overtime(
+            int overtimeId,
+            int employeeId,
+            LocalDate date,
+            double hours,
+            String reason,
+            String status) {
+
+        this.overtimeId = overtimeId;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.hours = hours;
+        this.reason = reason;
+        this.status = status;
+    }
+
+    public int getOvertimeId() {
+        return overtimeId;
+    }
+
+    public void setOvertimeId(int overtimeId) {
+        this.overtimeId = overtimeId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
