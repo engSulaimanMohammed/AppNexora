7bfbef3a7540ecae8ca25db7ad91bbba00812918
mohammed <mooodo150@gmail.com>
2026-08-13 10:24:02 +0400

    feat(server):I add override

---
+++ src/main/java/org/example/Utils/AccountAccessServiceImpl.java
@@ -0,0 +1,11 @@
+package org.example.Utils;
+
+import org.example.Interfaces.AccountAccessService;
+
+public class AccountAccessServiceImpl implements AccountAccessService {
+
+    @Override
+    public void updateUserRole(Long employeeId, String newRole) {
+
+    }
+}
