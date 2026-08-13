e18a0227c7b6ba0c4a67cceda713b9762560890e
mohammed said <mohed.said77@gmail.com>
2026-08-13 09:14:00 +0400

    use this method to all object

--- src/main/java/org/example/Entities/Attendance.java
+++ src/main/java/org/example/Entities/Attendance.java
@@ -10,4 +10,21 @@ public class Attendance {
     private LocalDate date;
     private LocalTime checkIn;
     private LocalTime checkOut;
+    private String status;
+
+    public Attendance(
+            int id,
+            int employeeId,
+            LocalDate date,
+            LocalTime checkIn,
+            LocalTime checkOut,
+            String status) {
+
+        this.id = id;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.status = status;
     }
+
