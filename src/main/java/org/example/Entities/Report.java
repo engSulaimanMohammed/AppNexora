879f2f11190e4ab5a4d841a2db268322ee17e6c7
Rashid <alshiblir650@gmail.com>
2026-08-13 08:52:22 +0400

    Create Report entity

---
+++ src/main/java/org/example/Entities/Report.java
@@ -0,0 +1,14 @@
+package org.example.Entities;
+
+public class Report {
+
+    private String title;
+    private String content;
+
+    public Report(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
