b7eb2a760a2f1ea6a578c29e590f4ee28efdf2d0
Rashid <alshiblir650@gmail.com>
2026-08-13 09:13:40 +0400

    Add toString method to AuditTrail

--- src/main/java/org/example/Entities/AuditTrail.java
+++ src/main/java/org/example/Entities/AuditTrail.java
@@ -27,4 +27,10 @@ public class AuditTrail {
     }
 
 
+    @Override
+    public String toString() {
+        return "[" + timestamp + "] " +
+                username + " -> " + action;
+    }
 }
+
