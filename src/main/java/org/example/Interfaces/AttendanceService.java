742b7d7b5a516d48bf1857b6952cfc2161054f62
mohammed <mooodo150@gmail.com>
2026-08-13 10:06:03 +0400

    feat(server): add override

--- src/main/java/org/example/Interfaces/AttendanceService.java
+++ src/main/java/org/example/Interfaces/AttendanceService.java
@@ -1,4 +1,17 @@
 package org.example.Interfaces;
 
-public interface AttendanceService {
+import org.example.Entities.AttendanceAdmin;
+
+public interface AttendanceService implements AttendanceAdmin {
+    @Override
+    public void recordClockIn(Long employeeId) {
+        // Implementation goes here.
+    }
+
+    @Override
+    public Attendance getAttendance(Long employeeId) {
+        // Implementation goes here.
+        return null;
+    }
+
 }
