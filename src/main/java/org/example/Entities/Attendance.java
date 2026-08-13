fab211f43f1a31dd956a7d68b95cc583ec3ad835
mohammed said <mohed.said77@gmail.com>
2026-08-13 09:17:45 +0400

    store status  to get and set and retuen for get to have value

--- src/main/java/org/example/Entities/Attendance.java
+++ src/main/java/org/example/Entities/Attendance.java
@@ -68,3 +68,11 @@ public class Attendance {
         this.checkOut = checkOut;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
